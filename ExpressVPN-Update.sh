849f6f58324a3af8ae6c2d3af810e71dcb0ccde4
J0n3s0fSt33l <91816393+J0n3s0fSt33l@users.noreply.github.com>
2022-07-28 00:24:29 -0400

    Update ExpressVPN-Update.sh

--- ExpressVPN-Update.sh
+++ ExpressVPN-Update.sh
@@ -1,38 +1,35 @@
-#! /bin/bash
+#!/bin/bash
 
 #System update and cleanup.
 apt update -y
-apt dist-upgrade -yy
+apt dist-upgrade -y
 apt autoclean 
 apt autoremove
 
-#
-MY_VERSION=$(expressvpn --version | awk '{print $3}' | awk -F '-' '{print $1}')
+my_ver=$(expressvpn --version | awk '{print $3}' | awk -F '-' '{print $1}')
 
-/usr/bin/curl -o xvp.txt --silent https://www.expressvpn.com/latest#linux || echo "Unable to connect to expressvpn.com. Check your network connection and try again"
+/usr/bin/curl -o xvp.txt --silent 'https://www.expressvpn.com/latest#linux' || echo "Unable to connect to expressvpn.com. Check your network connection and try again"
 
-CURRENT_VERSION=$(grep '_amd64.deb">Download' < xvp.txt | awk -F "_" '{print $2}' | awk -F "-" '{print $1}')
+current_ver=$(grep '_amd64.deb">Download' < xvp.txt | awk -F "_" '{print $2}' | awk -F "-" '{print $1}')
 
 #Compare your current version of Expressvpn with the one listed on the website
-if [[ "$MY_VERSION" == "$CURRENT_VERSION" ]]; then
-	echo "ExpressVPN is up to date."
-	
+if [[ "$my_ver" == "$current_ver" ]]; then
+	echo "ExpressVPN is up to date."	
 else
 
 #Creates a variable for the download link
-	DOWNLOAD=$(grep '_amd64.deb">Download' < xvp.txt | awk '{print $(NF)}' | awk -F '"' '{print $2}' )
+	download=$(grep '_amd64.deb">Download' < xvp.txt | awk '{print $(NF)}' | awk -F '"' '{print $2}' )
 #Creates a variable for the .deb file name
-	DEB_FILE=$(echo "$DOWNLOAD" | awk -F '/' '{print $6}') 
+	deb_file=$(echo "$download" | awk -F '/' '{print $6}') 
 #Creates a variable for the current version of Firefox
-	FF_VERSION=$(sudo -u akali /usr/bin/firefox -v | awk '{print $3}' | awk -F '.' '{print $1}')
+	ff_ver=$(sudo -u akali /usr/bin/firefox -v | awk '{print $3}' | awk -F '.' '{print $1}')
 #Creates a variable for a user agent for use with wget
-	USER_AGENT='Mozilla/5.0 (X11; Linux x86_64; rv:'$FF_VERSION".0) Gecko/20100101 Firefox/"$FF_VERSION.0
-	
+	user_agent='Mozilla/5.0 (X11; Linux x86_64; rv:'$ff_ver".0) Gecko/20100101 Firefox/"$ff_ver.0
 	echo "ExpressVPN is out of date. Updating now."
 	echo "Downloading update file."
-	/usr/bin/wget -q --user-agent "$USER_AGENT" "$DOWNLOAD" || echo "Unable to Download update file." 
+	/usr/bin/wget -q --user-agent "$user_agent" "$download" || echo "Unable to download update file." 
 	echo "Installing update"
-	rm xvp.txt && dpkg -i "$DEB_FILE" || echo "Update installation NOT successful."
+	rm xvp.txt && dpkg -i "$deb_file" || echo "Update installation NOT successful."
 fi
 
 read -rsp "Press any key to continue..."
